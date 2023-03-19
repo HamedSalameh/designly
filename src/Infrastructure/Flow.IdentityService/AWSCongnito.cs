27a0c21c5b073bc90fb08979cc3504128cf4aa83
Hamed Salameh <hamedsalami@gmail.com>
2023-03-19 06:03:48 +0200

    initial creation

---
+++ src/Infrastructure/Flow.IdentityService/AWSCongnito.cs
@@ -0,0 +1,50 @@
+﻿using Amazon.CognitoIdentityProvider;
+using Amazon.Extensions.CognitoAuthentication;
+using Flow.SharedKernel.Interfaces;
+using Flow.SharedKernel.Models;
+
+namespace Flow.IdentityService
+{
+    public class AWSCongnito : IIdentityService
+    {
+        private readonly AmazonCognitoIdentityProviderClient _provider;
+        private readonly CognitoUserPool _userPool;
+        private string AppClientId = "37jb2mi88p635sl5k6si2metde";
+
+        public AWSCongnito()
+        {
+            _provider =
+                new AmazonCognitoIdentityProviderClient(new Amazon.Runtime.AnonymousAWSCredentials());
+            _userPool = new CognitoUserPool("us-east-1_t7SXW9J4E", AppClientId, _provider);
+        }
+
+        public async Task<User> LoginAsync(string username, string password)
+        {
+            var authenticatedUser = await AuthenticateUserAsync(username, password);
+
+            return authenticatedUser;
+        }
+
+        private async Task<User> AuthenticateUserAsync(string emailAddress, string password)
+        {
+            CognitoUser user = new CognitoUser(emailAddress, AppClientId, _userPool, _provider);
+            InitiateSrpAuthRequest authRequest = new InitiateSrpAuthRequest()
+            {
+                Password = password
+            };
+
+            AuthFlowResponse authResponse = await user.StartWithSrpAuthAsync(authRequest);
+            var result = authResponse.AuthenticationResult;
+
+            var authenticatedUser = new User(user.UserID, user.Username);
+            authenticatedUser.ExpiresIn = result.ExpiresIn;
+            authenticatedUser.AccessToken = result.AccessToken;
+            authenticatedUser.IdToken = result.IdToken;
+            authenticatedUser.RefreshToken = result.RefreshToken;
+            authenticatedUser.TokenType = result.TokenType;
+
+            // return new Tuple<CognitoUser, AuthenticationResultType>(user, result);
+            return authenticatedUser;
+        }
+    }
+}
---
+++ src/clients/clients.api/Controllers/clients.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Mime;
+using Flow.SharedKernel.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace clients.api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Clients : ControllerBase
+{
+    private readonly IIdentityService identityService;
+
+    public Clients(IIdentityService identityService)
+    {
+        this.identityService = identityService ?? throw new ArgumentNullException(nameof(identityService));
+    }
+
+    [HttpPost]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> Login()
+    {
+        var username = "hamedsalami@gmail.com";
+        var password = "p@55w0rd!";
+
+        var response = await identityService.LoginAsync(username, password);
+
+        return Ok(response);
+    }
+}
