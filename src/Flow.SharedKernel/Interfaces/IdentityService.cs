b524407645d81af8bb1fa88451d41efb527ebd3d
Hamed Salameh <hamedsalami@gmail.com>
2023-03-19 06:03:48 +0200

    initial creation

---
+++ src/Flow.SharedKernel/Interfaces/IdentityService.cs
@@ -0,0 +1,9 @@
+﻿using Flow.SharedKernel.Models;
+
+namespace Flow.SharedKernel.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<User> LoginAsync(string username, string password);
+    }
+}
