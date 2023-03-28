ed4b84c6c749671068e801986de63aa84b2462d5
Hamed Salameh <hamedsalami@gmail.com>
2023-03-28 16:27:03 +0300

    wip

--- src/Flow.SharedKernel/Interfaces/IIdentityService.cs
+++ src/Flow.SharedKernel/Interfaces/IIdentityService.cs
@@ -3,6 +3,7 @@
     public interface IIdentityService
     {
         Task<ITokenResponse?> LoginAsync(string username, string password, CancellationToken cancellationToken);
+        Task<ITokenResponse?> LoginJwtAsync(string username, string password, CancellationToken cancellationToken);
         Task<bool> SignoutAsync(string accessToken, CancellationToken cancellation);
     }
 }
