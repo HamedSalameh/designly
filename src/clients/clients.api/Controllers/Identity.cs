8eb4bd5af3deec76c8cace7fa937437113f5ae76
Hamed Salameh <hamedsalami@gmail.com>
2023-03-31 09:01:04 +0300

    updating controllers

--- src/clients/clients.api/Controllers/Identity.cs
+++ src/clients/clients.api/Controllers/Identity.cs
@@ -58,6 +58,31 @@ namespace Clients.API.Controllers
             return Ok();
         }
 
+        [HttpPost]
+        [Route("/Refresh")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Refresh([FromBody] RefreshTokenRequest refreshTokenRequest, CancellationToken cancellation)
+        {
+            if (refreshTokenRequest == null || string.IsNullOrEmpty(refreshTokenRequest.RefreshToken))
+            {
+                return BadRequest();
+            }
+
+            var refreshTokenCommand = new RefreshTokenCommand()
+            {
+                RefreshToken = refreshTokenRequest.RefreshToken
+            };
+
+            var response = await _mediator.Send(refreshTokenCommand, cancellation).ConfigureAwait(false);
+
+            return Ok(response);
+        }
+
         [HttpGet]
         public IActionResult TestAuthentication()
         {
