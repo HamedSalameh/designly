7e3ff4d81fdebf44dff63b26b424df6ebcbbadae
Hamed Salameh <hamedsalami@gmail.com>
2023-04-09 15:45:59 +0300

    client wip

--- src/WebApp/src/app/app-routing.module.ts
+++ src/WebApp/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationGuard } from './core/guards/authentication.guard';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    canActivate: [AuthenticationGuard],
+    children: [
+      { path: 'home', component: HomeComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
