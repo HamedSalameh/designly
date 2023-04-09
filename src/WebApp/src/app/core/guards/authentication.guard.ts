efd6c399a75467d5f70a0c48b1793aa852fa745b
Hamed Salameh <hamedsalami@gmail.com>
2023-04-09 15:45:59 +0300

    client wip

---
+++ src/WebApp/src/app/core/guards/authentication.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationGuard implements CanActivate {
+
+  // Set isAuthenticated to true for authenticated users and false for unauthenticated users
+  isAuthenticated = false;
+
+  constructor(private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.isAuthenticated) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+  
+}
