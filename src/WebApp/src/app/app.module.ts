71bf258f64b06d405720770f3ad5ef8fc780f9c4
Hamed Salameh <hamedsalami@gmail.com>
2023-04-10 15:31:49 +0300

    login page

--- src/WebApp/src/app/app.module.ts
+++ src/WebApp/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/WebApp/src/app/login/login.component.ts
+++ src/WebApp/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  signUp() {
+    console.log('Signup is not yet implemented');
+  }
+
+  signIn() {
+    console.log('Signin is not yet implemented');
+  }
+
+  signInWithGoogle() {
+    console.log('Sign In with Google');
+  }
+
+  requestPasswordReset() {
+    console.log('Request Password Reset');
+  }
 
 }
