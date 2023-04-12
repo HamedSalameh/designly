3795a7947b320b8bcd08a53b2a00d76ca5f28501
Hamed Salameh <hamedsalami@gmail.com>
2023-04-12 13:07:19 +0300

    Domain

---
+++ src/Clients/Clients.Domain/Entities/Client.cs
@@ -0,0 +1,13 @@
+﻿
+using Clients.Domain.ValueObjects;
+
+namespace Clients.Domain.Entities
+{
+    public class Client : Entity
+    {
+        public string FirstName { get; set; }
+        public string FamilyName { get; set; }
+
+        public Address Address { get; set; }
+    }
+}
