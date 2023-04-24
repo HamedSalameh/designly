85392822d2e7abeda80c2d14800a28bd994b3cb6
Hamed Salameh <hamedsalami@gmail.com>
2023-04-24 08:41:24 +0300

    clients-db-liquibase

---
+++ src/Clients/Clients.DB/changesets/changeset-02-create-clients-table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE IF NOT EXISTS clients (
+  id UUID PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  family_name VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  building_number VARCHAR(255) NOT NULL,
+  address_lines JSONB,
+  primary_phone_number VARCHAR(255) NOT NULL,
+  secondary_phone_number VARCHAR(255),
+  email_address VARCHAR(255),
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT now()
+)
