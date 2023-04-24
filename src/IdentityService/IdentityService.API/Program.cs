e319dcfd867a2765408f799f84049d158c5c4b58
Hamed Salameh <hamedsalami@gmail.com>
2023-04-24 18:02:08 +0300

    update & wip docker

--- src/IdentityService/IdentityService.API/Program.cs
+++ src/IdentityService/IdentityService.API/Program.cs
@@ -7,91 +7,98 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System.Net.Mime;
 using System.Text.Json;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Configuration
-    .SetBasePath(AppContext.BaseDirectory)
-    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-
-var configuration = builder.Configuration;
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddApiVersioning(v =>
+public class Program
 {
-    v.AssumeDefaultVersionWhenUnspecified = true;
-    v.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
-    v.ReportApiVersions = true;
-    v.ApiVersionReader = ApiVersionReader.Combine(
-        new QueryStringApiVersionReader("api-version"),
-        new HeaderApiVersionReader("api-version"),
-        new MediaTypeApiVersionReader("ver"));
-});
-builder.Services.AddEndpointsApiExplorer();
-
-// Enabled authentication
-builder.Services.AddJwtBearerConfig(configuration);
-builder.Services.AddAuthorization();
-
-// Configure Swagger
-builder.Services.ConfigureSecuredSwagger();
-builder.Services.ConfigureCors();
-
-// Configure Services
-builder.Services.AddApplication(configuration);
-builder.Services.AddIdentityService(configuration);
-
-// Configure Health checks
-builder.Services.AddHealthChecks();
-
-builder.Services.AddControllers();
-
-var app = builder.Build();
-
-app.UseRouting();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-// Configure CORS middleware
-app.UseCors("DevelopmentCors");
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-// the call to UserAuthorization should appeat between UseRouting and UseEndpoints
-app.UseAuthorization();
-
-app.UseEndpoints((Action<IEndpointRouteBuilder>)(endpoints =>
-{
-    ConfigureHealthChecksRouting(endpoints);
-
-    endpoints.MapControllers();
-}));
-
-app.Run();
-
-void ConfigureHealthChecksRouting(IEndpointRouteBuilder endpoints)
-{
-    endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+    public static void Main(string[] args)
     {
-        AllowCachingResponses = false
-    });
+        var builder = WebApplication.CreateBuilder(args);
 
-    endpoints.MapHealthChecks("/health-details",
-        new HealthCheckOptions
+        builder.Configuration
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddEnvironmentVariables()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        var configuration = builder.Configuration;
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddApiVersioning(v =>
         {
-            ResponseWriter = async (context, report) =>
-            {
-                var result = JsonSerializer.Serialize(
-                    new
-                    {
-                        status = report.Status.ToString(),
-                        monitors = report.Entries.Select(e => new { key = e.Key, value = Enum.GetName(typeof(HealthStatus), e.Value.Status) })
-                    });
-                context.Response.ContentType = MediaTypeNames.Application.Json;
-                await context.Response.WriteAsync(result);
-            }
+            v.AssumeDefaultVersionWhenUnspecified = true;
+            v.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+            v.ReportApiVersions = true;
+            v.ApiVersionReader = ApiVersionReader.Combine(
+                new QueryStringApiVersionReader("api-version"),
+                new HeaderApiVersionReader("api-version"),
+                new MediaTypeApiVersionReader("ver"));
         });
+        builder.Services.AddEndpointsApiExplorer();
+
+        // Enabled authentication
+        builder.Services.AddJwtBearerConfig(configuration);
+        builder.Services.AddAuthorization();
+
+        // Configure Swagger
+        builder.Services.ConfigureSecuredSwagger();
+        builder.Services.ConfigureCors();
+
+        // Configure Services
+        builder.Services.AddApplication(configuration);
+        builder.Services.AddIdentityService(configuration);
+
+        // Configure Health checks
+        builder.Services.AddHealthChecks();
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        app.UseRouting();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        // Configure CORS middleware
+        app.UseCors("DevelopmentCors");
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        // the call to UserAuthorization should appeat between UseRouting and UseEndpoints
+        app.UseAuthorization();
+
+        app.UseEndpoints((Action<IEndpointRouteBuilder>)(endpoints =>
+        {
+            ConfigureHealthChecksRouting(endpoints);
+
+            endpoints.MapControllers();
+        }));
+
+        app.Run();
+
+        void ConfigureHealthChecksRouting(IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+            {
+                AllowCachingResponses = false
+            });
+
+            endpoints.MapHealthChecks("/health-details",
+                new HealthCheckOptions
+                {
+                    ResponseWriter = async (context, report) =>
+                    {
+                        var result = JsonSerializer.Serialize(
+                            new
+                            {
+                                status = report.Status.ToString(),
+                                monitors = report.Entries.Select(e => new { key = e.Key, value = Enum.GetName(typeof(HealthStatus), e.Value.Status) })
+                            });
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
+                });
+        }
+    }
 }
