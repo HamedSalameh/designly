56ba6e95649e31c53d1dd2bb3b65598c5c45df06
Hamed Salameh <hamedsalami@gmail.com>
2023-04-24 08:41:24 +0300

    clients-db-liquibase

--- src/Clients/Clients.DB/run-pgdocker.cmd
+++ src/Clients/Clients.DB/run-pgdocker.cmd
@@ -1,1 +1,1 @@
-docker run --name postgres -e POSTGRES_PASSWORD=admin -p 5432:5432 -d postgres
+docker run --name postgres -e POSTGRES_PASSWORD=admin -e POSTGRES_DB=designlydb -p 5432:5432 -d postgres
