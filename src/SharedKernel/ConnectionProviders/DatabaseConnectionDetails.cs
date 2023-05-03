4d4be7913cbf58006607a7a78c6693d79062a747
Hamed Salameh <hamedsalami@gmail.com>
2023-05-03 10:31:49 +0300

    prep for packaging the shared infra

--- src/SharedKernel/ConnectionProviders/DatabaseConnectionDetails.cs
+++ src/SharedKernel/ConnectionProviders/DatabaseConnectionDetails.cs
@@ -4,22 +4,16 @@ namespace SharedKernel.ConnectionProviders
 {
     public class DatabaseConnectionDetails
     {
-        [JsonPropertyName("db_name")]
-        public string Database { get; set; }
+        [JsonPropertyName("db_name")] public string Database { get; set; } = "";
 
-        [JsonPropertyName("hostname")]
-        public string Hostname { get; set; }
+        [JsonPropertyName("hostname")] public string Hostname { get; set; } = "";
 
-        [JsonPropertyName("password")]
-        public string Password { get; set; }
+        [JsonPropertyName("password")] public string Password { get; set; } = "";
 
-        [JsonPropertyName("port")]
-        public int Port { get; set; }
+        [JsonPropertyName("port")] public int Port { get; set; } = 0;
 
-        [JsonPropertyName("uri")]
-        public string Uri { get; set; }
+        [JsonPropertyName("uri")] public string Uri { get; set; } = "";
 
-        [JsonPropertyName("username")]
-        public string Username { get; set; }
+        [JsonPropertyName("username")] public string Username { get; set; } = "";
     }
 }
