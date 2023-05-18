2ae01195e735234484d91456fb394dca8dfeea5f
Hamed Salameh <hamedsalami@gmail.com>
2023-05-18 23:06:34 +0300

    login ui

--- src/WebApp/src/app/authentication/auth-interceptor.service.ts
+++ src/WebApp/src/app/authentication/auth-interceptor.service.ts
@@ -1,26 +1,31 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthInterceptorService implements HttpInterceptor {
+  constructor() {}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const idToken = localStorage.getItem('id_token');
 
-  constructor() { }
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const idToken = localStorage.getItem("id_token");
+    if (idToken) {
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization', 'Bearer ' + idToken),
+      });
 
-        if (idToken) {
-            const cloned = req.clone({
-                headers: req.headers.set("Authorization",
-                    "Bearer " + idToken)
-            });
-
-            return next.handle(cloned);
-        }
-        else {
-            return next.handle(req);
-        }
+      return next.handle(cloned);
+    } else {
+      return next.handle(req);
+    }
   }
 }
