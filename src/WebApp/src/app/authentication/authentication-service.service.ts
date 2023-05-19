b465b1f4f5bfc73aee9d65fcb25d2b9af1760e45
Hamed Salameh <hamedsalami@gmail.com>
2023-05-19 08:20:18 +0300

    Authenitcation service & guards

--- src/WebApp/src/app/authentication/authentication-service.service.ts
+++ src/WebApp/src/app/authentication/authentication-service.service.ts
@@ -1,56 +1,67 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import * as moment from 'moment';
-import { Observable } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 import { share, shareReplay, tap } from 'rxjs/operators';
 import { SigninRequest } from './models/signin-request.model';
 import { SigninResponse } from './models/signin-response.model';
 
+export const access_token_key = 'access_token';
+export const expires_at_key = 'expires_at';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
+  //serviceAddress: string = "https://localhost:7246";
+  serviceAddress: string = 'http://localhost:3000/api';
 
-  serviceAddress: string = "https://localhost:7246";
+  private isUserAuthenticated: boolean = false;
 
-  constructor(private httpClient: HttpClient) {
-  }
+  constructor(private httpClient: HttpClient) {}
 
   public signIn(signinRequest: SigninRequest): Observable<any> {
-
     const formData: FormData = new FormData();
     formData.append('Username', signinRequest.username);
     formData.append('Password', signinRequest.password);
 
     const httpOptions = {
       headers: new HttpHeaders({
-        'Accept': 'application/json'
-      })
+        Accept: 'application/json',
+      }),
     };
 
-    return this.httpClient.post<SigninResponse>(`${this.serviceAddress}/signin`, formData, httpOptions).pipe(
-      tap(res => this.setSession(res)),
-      shareReplay()
-    );
-      
+    return this.httpClient
+      .post<SigninResponse>(
+        `${this.serviceAddress}/signin`,
+        formData,
+        httpOptions
+      )
+      .pipe(
+        tap((res) => this.setSession(res)),
+        shareReplay()
+      );
   }
 
-  public test(): Observable<any> {
-    let access = localStorage.getItem('access_token');
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Accept': 'application/json',
-        'Authorization': 'Bearer ' + access
-      })
-    };
-
-    return this.httpClient.get<SigninResponse>(`${this.serviceAddress}/test`, httpOptions);
+  public isAuthenticated() {
+    const token = localStorage.getItem(access_token_key);
+    if (!token || this.tokenExpired(token)) {
+      return false;
+    }
+    return true;
   }
 
   private setSession(authResult: SigninResponse) {
-    const expiresAt = moment().add(authResult.idToken,'second');
+    const expiresAt = moment().add(authResult.idToken, 'second');
 
-        localStorage.setItem('access_token', authResult.accessToken);
-        localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
+    localStorage.setItem(access_token_key, authResult.accessToken);
+    localStorage.setItem(expires_at_key, JSON.stringify(expiresAt.valueOf()));
+
+    this.isUserAuthenticated = true;
+  }
+
+  private tokenExpired(token: string) {
+    const expiry = JSON.parse(atob(token.split('.')[1])).exp;
+    return Math.floor(new Date().getTime() / 1000) >= expiry;
   }
 }
