4a14ee9add669a7f6f166b595a30b7fd741b5e37
Hamed Salameh <hamedsalami@gmail.com>
2023-05-26 08:25:31 +0300

    client selection

--- src/WebApp/src/app/shared/components/table/table.component.ts
+++ src/WebApp/src/app/shared/components/table/table.component.ts
@@ -1,19 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
 })
 export class TableComponent {
+  
+  onRowSelect($event: any) {
+    this.rowSelected.emit($event.data);
+  }
+
+  selectedItem: any;
 
   @Input()
   data: any[] = [];
-  
+
   @Input()
   cols: any[] = [];
 
-  constructor() {
-  }
+  @Output()
+  rowSelected: EventEmitter<any> = new EventEmitter();
 
+  constructor() {}
 }
