861c90f24710cad3ec1a614a9a925575d53cd672
Hamed Salameh <hamedsalami@gmail.com>
2023-05-27 08:40:55 +0300

    client-card-wip

---
+++ src/WebApp/src/app/clients/components/clients-management/clients-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clients-management',
+  templateUrl: './clients-management.component.html',
+  styleUrls: ['./clients-management.component.scss']
+})
+export class ClientsManagementComponent {
+
+}
