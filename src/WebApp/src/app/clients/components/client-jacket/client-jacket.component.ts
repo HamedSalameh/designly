82bc87ed96389eb75f2715053ac62ac751a0d08a
Hamed Salameh <hamedsalami@gmail.com>
2023-05-27 08:40:55 +0300

    client-card-wip

---
+++ src/WebApp/src/app/clients/components/client-jacket/client-jacket.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-jacket',
+  templateUrl: './client-jacket.component.html',
+  styleUrls: ['./client-jacket.component.scss']
+})
+export class ClientJacketComponent {
+
+}
