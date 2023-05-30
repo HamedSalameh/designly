194b758876f41bbe528309de1ed7a21c2e56dcf2
Hamed Salameh <hamedsalami@gmail.com>
2023-05-30 16:21:19 +0200

    adding localization (#6)

--- src/WebApp/src/app/side-nav/side-nav/side-nav.component.ts
+++ src/WebApp/src/app/side-nav/side-nav/side-nav.component.ts
@@ -5,12 +5,17 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent {
+  dashboardTitle = $localize`:@@sideNav.Dashboard:Dashboard`;
+  projectsTitle = $localize`:@@sideNav.Projects:Projects`;
+  tasksTitle = $localize`:@@sideNav.Tasks:Tasks`;
+  clientsTitle = $localize`:@@sideNav.Clients:Clients`;
+  communicationTitle = $localize`:@@sideNav.Communication:Communication`;
+  analyticsTitle = $localize`:@@sideNav.Analytics:Analytics`;
 
-  constructor(private router: Router, private domSanitizer: DomSanitizer) {
-  }
+  constructor(private router: Router, private domSanitizer: DomSanitizer) {}
 
   navigateToDashboard() {
     this.router.navigate(['/home/dashboard']);
@@ -19,5 +24,4 @@ export class SideNavComponent {
   navigateToClient() {
     this.router.navigate(['/home/clients']);
   }
-
 }
