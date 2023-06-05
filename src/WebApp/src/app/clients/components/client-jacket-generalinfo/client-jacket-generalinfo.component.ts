58d01f59f9b926558fc0f999c37457e4e0630363
Hamed Salameh <hamedsalami@gmail.com>
2023-06-05 09:34:24 +0300

    state and flow refactor

--- src/WebApp/src/app/clients/components/client-jacket-generalinfo/client-jacket-generalinfo.component.ts
+++ src/WebApp/src/app/clients/components/client-jacket-generalinfo/client-jacket-generalinfo.component.ts
@@ -1,13 +1,16 @@
 import { Component, Input } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { ClientState } from 'src/app/state/client-state/client-state.state';
 import { Client } from '../../models/client.model';
+import { ClientsServiceService } from '../../services/clients-service.service';
 
 @Component({
   selector: 'app-client-jacket-generalinfo',
   templateUrl: './client-jacket-generalinfo.component.html',
-  styleUrls: ['./client-jacket-generalinfo.component.scss']
+  styleUrls: ['./client-jacket-generalinfo.component.scss'],
 })
 export class ClientJacketGeneralinfoComponent {
-
   @Input() client: Client | undefined;
 
 }
--- src/WebApp/src/app/clients/components/clients/clients.component.ts
+++ src/WebApp/src/app/clients/components/clients/clients.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { map, tap } from 'rxjs';
 import { SelectClient } from 'src/app/state/client-state/client-state.actions';
@@ -10,6 +10,9 @@ import { ClientsServiceService } from '../../services/clients-service.service';
   styleUrls: ['./clients.component.scss'],
 })
 export class ClientsComponent {
+
+  @Output() SelectRow: EventEmitter<string> = new EventEmitter<string>();
+
   columnsDefinition = [
     {
       ColumnHeader: $localize`:@@clients.Headers.FirstName:FirstName`,
@@ -61,24 +64,28 @@ export class ClientsComponent {
       .subscribe((clients) => (this.tableData = clients));
   }
 
-  onRowSelect($event: any): void {
-    console.log($event);
-    const client = {
-      Id: $event['Id'],
-      FirstName: $event['FirstName'],
-      FamilyName: $event['FamilyName'],
-      TenantId: $event['TenantId'],
-      Address: {
-        City: $event['City'],
-        Street: $event['Street'],
-        BuildingNumber: $event['BuildingNumber'],
-      },
-      ContactDetails: {
-        PrimaryPhoneNumber: $event['primaryPhoneNumber'],
-        EmailAddress: $event['Email'],
-      },
-    };
-    this.store.dispatch(new SelectClient(client));
+  onRowSelect(event: any): void {
+    // TODO: Instead of building the client from the table data, should we get the client from the store or local storage?
+    // If we get the client from the store or local storage, we need to make sure that the client is up to date.
+    // then getting the client from the store or local storage will be the using @Select decorator
+
+    // const client = {
+    //   Id: $event['Id'],
+    //   FirstName: $event['FirstName'],
+    //   FamilyName: $event['FamilyName'],
+    //   TenantId: $event['TenantId'],
+    //   Address: {
+    //     City: $event['City'],
+    //     Street: $event['Street'],
+    //     BuildingNumber: $event['BuildingNumber'],
+    //   },
+    //   ContactDetails: {
+    //     PrimaryPhoneNumber: $event['primaryPhoneNumber'],
+    //     EmailAddress: $event['Email'],
+    //   },
+    // };
+    const selectedClientId: string = event?.Id || null;
+    this.SelectRow.emit(selectedClientId);
   }
 
   private mapClientToTableData(client: any) {
