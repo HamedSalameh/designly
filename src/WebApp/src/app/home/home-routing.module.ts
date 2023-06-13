ef58f5eb1880464564fe9effd4d2c7ce7900e412
Hamed Salameh <hamedsalami@gmail.com>
2023-06-13 17:00:45 +0200

    Adding breadcrumbs (service & component( (#9)
    * adding breadcrumbs
    
    * adding breadcrumbs
    
    * fixing translation
    
    * breadcrumbs

--- src/WebApp/src/app/home/home-routing.module.ts
+++ src/WebApp/src/app/home/home-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../home/components/home/home.component';
 
+const localizedDashboardLabel = $localize`:@@Global.Nav.Dashboard:Dashboard`;
+const localizedClientsLabel = $localize`:@@Global.Nav.Clients:Clients`;
+
 const routes: Routes = [
   {
     path: '',
@@ -9,11 +12,13 @@ const routes: Routes = [
     children: [
         {
         path: 'dashboard',
+        data: { breadcrumb: localizedDashboardLabel },
         loadChildren: () =>
             import('../dashboard/dashboard.module').then((m) => m.DashboardModule)
         },
       {
         path: 'clients',
+        data: { breadcrumb: localizedClientsLabel },
         loadChildren: () =>
           import('../clients/clients.module').then((m) => m.ClientsModule)
       },
--- src/WebApp/src/app/side-nav/side-nav/side-nav.component.ts
+++ src/WebApp/src/app/side-nav/side-nav/side-nav.component.ts
@@ -8,12 +8,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent {
-  dashboardTitle = $localize`:@@sideNav.Dashboard:Dashboard`;
-  projectsTitle = $localize`:@@sideNav.Projects:Projects`;
-  tasksTitle = $localize`:@@sideNav.Tasks:Tasks`;
-  clientsTitle = $localize`:@@sideNav.Clients:Clients`;
-  communicationTitle = $localize`:@@sideNav.Communication:Communication`;
-  analyticsTitle = $localize`:@@sideNav.Analytics:Analytics`;
+  dashboardTitle = $localize`:@@Global.Nav.Dashboard:Dashboard`;
+  projectsTitle = $localize`:@@Global.Nav.Projects:Projects`;
+  tasksTitle = $localize`:@@Global.Nav.Tasks:Tasks`;
+  clientsTitle = $localize`:@@Global.Nav.Clients:Clients`;
+  communicationTitle = $localize`:@@Global.Nav.Communication:Communication`;
+  analyticsTitle = $localize`:@@Global.Nav.Analytics:Analytics`;
 
   constructor(private router: Router, private domSanitizer: DomSanitizer) {}
 
