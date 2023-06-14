97fed23429f1b5ae8c7f6f6b45157d33ab0e2844
Hamed Salameh <hamedsalami@gmail.com>
2023-06-14 17:02:05 +0300

    Adding filtering with chips

--- src/WebApp/src/app/shared/shared.module.ts
+++ src/WebApp/src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconButtonComponent } from './components/icon-button/icon-button.component';
 import { TableComponent } from './components/table/table.component';
-import { TableModule } from 'primeng/table';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormInputComponent } from './components/form-input/form-input.component';
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+
+import { TableModule } from 'primeng/table';
+import { ChipsModule } from 'primeng/chips';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.componen
     FormsModule,
     ReactiveFormsModule,
 
-    TableModule
+    TableModule,
+    ChipsModule,
   ],
   exports: [
     IconButtonComponent,
