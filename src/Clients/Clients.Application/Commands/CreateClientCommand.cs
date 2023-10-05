9f6938dd3346a87fe14cfb7d63e8abc05b962797
Hamed Salameh <hamedsalami@gmail.com>
2023-10-05 17:50:30 +0300

    Adding Validation Pipeline Behavior

--- src/Clients/Clients.Application/Commands/CreateClientCommand.cs
+++ src/Clients/Clients.Application/Commands/CreateClientCommand.cs
@@ -5,33 +5,33 @@ using Microsoft.Extensions.Logging;
 
 namespace Clients.Application.Commands
 {
-    public record CreateClientCommand(Client DraftClient) : IRequest<Guid>;
+    public record CreateClientCommand(Client Client) : IRequest<Guid>;
 
     public class CreateClientCommandHandler : IRequestHandler<CreateClientCommand, Guid>
     {
-        private readonly ILogger<CreateClientCommandHandler> logger;
-        private readonly IUnitOfWork unitOfWork;
+        private readonly ILogger<CreateClientCommandHandler> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
         public CreateClientCommandHandler(ILogger<CreateClientCommandHandler> logger, IUnitOfWork unitOfWork)
         {
-            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            this.unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+            this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this._unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
         }
 
         public async Task<Guid> Handle(CreateClientCommand request, CancellationToken cancellationToken)
         {
-            var client = request.DraftClient;
+            var client = request.Client;
 
             try
             {
-                var clientId = await unitOfWork.ClientsRepository.CreateClientAsync(client, cancellationToken).ConfigureAwait(false);
-                logger.LogDebug("Created client: {clientId}", clientId);
+                var clientId = await _unitOfWork.ClientsRepository.CreateClientAsync(client, cancellationToken).ConfigureAwait(false);
+                _logger.LogDebug("Created client: {clientId}", clientId);
 
                 return clientId;
             }
             catch (Exception exception)
             {
-                logger.LogError(exception, "Could not create new client due to error: {exception.Message}", exception.Message);
+                _logger.LogError(exception, "Could not create new client due to error: {exception.Message}", exception.Message);
                 throw;
             }
         }
---
+++ src/Clients/Clients.Domain/Exceptions/ValidationException.cs
@@ -0,0 +1,35 @@
+﻿namespace Clients.Domain.Exceptions
+{
+    [Serializable]
+    public class ValidationException : Exception
+    {
+        // Represent business exception
+        // The exception data can passed as dictionary<string,string> or as a list of ValidationFailure
+
+        public ValidationException(string message)
+            : base(message)
+        {
+            this.Errors = new Dictionary<string, string>();
+        }
+
+        public ValidationException(string message, Exception inner)
+            : base(message, inner)
+        {
+            this.Errors = new Dictionary<string, string>();
+        }
+        
+        public ValidationException(string message, IDictionary<string, string> errors)
+            : base(message)
+        {
+            Errors = errors;
+        }
+
+        public ValidationException(IDictionary<string, string> errors) : this(
+            "One or more validation failures have occurred.")
+        {
+            this.Errors = errors;
+        }
+        
+        public IDictionary<string, string> Errors { get; }
+    }
+}
