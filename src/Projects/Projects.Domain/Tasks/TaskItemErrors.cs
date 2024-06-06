2aa1814bad695d50d185c0419c05916622070a19
Hamed Salameh <hamedsalami@gmail.com>
2024-06-06 13:24:28 +0300

    unit test and bug fixes

--- src/Projects/Projects.Domain/Tasks/TaskItemErrors.cs
+++ src/Projects/Projects.Domain/Tasks/TaskItemErrors.cs
@@ -5,5 +5,6 @@ namespace Projects.Domain.Tasks
     public static class TaskItemErrors
     {
         public static readonly Error TaskItemNotCreatedYet = new("task_item_not_created_yet", "Task item is not created yet");
+        public static readonly Error DueDateCannotBeInThePast = new("due_date_cannot_be_in_the_past", "Due date cannot be in the past");
     }
 }
--- src/Projects/Projects.Tests/Tasks/CreateTaskCommandHandlerTest.cs
+++ src/Projects/Projects.Tests/Tasks/CreateTaskCommandHandlerTest.cs
@@ -1,10 +1,13 @@
-﻿using LanguageExt.Common;
+﻿using Designly.Auth.Identity;
+using Designly.Base.Exceptions;
+using LanguageExt.Common;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Projects.Application.Builders;
 using Projects.Application.Features.CreateTask;
 using Projects.Application.LogicValidation;
 using Projects.Application.LogicValidation.Requests;
+using Projects.Domain.Tasks;
 using Projects.Infrastructure.Interfaces;
 
 namespace Projects.Tests.Tasks
@@ -16,6 +19,7 @@ namespace Projects.Tests.Tasks
         private readonly Mock<IBusinessLogicValidator> _businessLogicValidatorMock;
         private readonly Mock<ILogger<CreateTaskCommandHandler>> _loggerMock;
         private readonly Mock<ITaskItemBuilder> _taskItemBuilderMock;
+        private readonly Mock<ITenantProvider> _tenantProviderMock;
         private readonly CreateTaskCommandHandler _sut;
 
         public CreateTaskCommandHandlerTest()
@@ -24,6 +28,7 @@ namespace Projects.Tests.Tasks
             _businessLogicValidatorMock = new Mock<IBusinessLogicValidator>();
             _loggerMock = new Mock<ILogger<CreateTaskCommandHandler>>();
             _taskItemBuilderMock = new Mock<ITaskItemBuilder>();
+            _tenantProviderMock = new Mock<ITenantProvider>();
             _sut = new CreateTaskCommandHandler(_loggerMock.Object, _unitOfWorkMock.Object, _businessLogicValidatorMock.Object, _taskItemBuilderMock.Object);
         }
 
@@ -47,6 +52,48 @@ namespace Projects.Tests.Tasks
             // Assert
             // do not use shouldly here, it's not working with LanguageExt.Result
             Assert.That(result, Is.EqualTo(new Result<Guid>(projectValidationResult)));
+        }
+
+        [Test]
+        public async Task Handle_ShouldCreateTask_GetTaskIdFromRepo()
+        {
+            // Arrange
+            var request = new CreateTaskCommand
+            {
+                Name = "Task 1",
+                ProjectId = Guid.NewGuid(),
+                TenantId = Guid.NewGuid()
+            };
+
+            var generatedTaskId = Guid.NewGuid();
+
+            _businessLogicValidatorMock.Setup(x => x.ValidateAsync(It.IsAny<CreateTasksValidationRequest>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync((BusinessLogicException)null!);
+
+            _tenantProviderMock.Setup( x => x.GetTenantId())
+                .Returns(request.TenantId);
+
+            _unitOfWorkMock.Setup(x => x.TaskItemsRepository.AddAsync(It.IsAny<TaskItem>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(generatedTaskId);
+
+            // setup TaskItemBuilder
+            TaskItemBuilder taskItemBuilder = new TaskItemBuilder(_tenantProviderMock.Object);
+
+            var sut = new CreateTaskCommandHandler(_loggerMock.Object, _unitOfWorkMock.Object, _businessLogicValidatorMock.Object, taskItemBuilder);
+
+            // assert
+            var result = await sut.Handle(request, CancellationToken.None);
+
+            // Assert
+            Assert.That(result.IsSuccess);
+
+            // extract the value from the result
+            var taskId = result.Match(
+                               Succ: id => id,
+                               Fail: ex => Guid.Empty);
+
+            Assert.That(taskId, Is.Not.EqualTo(Guid.Empty));
+            Assert.That(taskId, Is.EqualTo(generatedTaskId));
         }
     }
 }
