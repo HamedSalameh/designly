7e8477ccc8d027befa869826ab9894bfbe5d3fcc
Hamed Salameh <hamedsalami@gmail.com>
2024-12-16 15:36:43 +0200

    wip

---
+++ src/WebApp/src/app/projects/components/asset-view/asset-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-view',
+  templateUrl: './asset-view.component.html',
+  styleUrls: ['./asset-view.component.scss']
+})
+export class AssetViewComponent {
+
+}
